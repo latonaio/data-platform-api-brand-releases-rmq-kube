498b99ff73b89f6d9ee8b7147df8f0813674ef42
misupopo <kokorononakaniitumo@yahoo.co.jp>
2024-05-07 16:57:47 +0900

    no message

---
+++ DPFM_API_Caller/requests/header.go
@@ -0,0 +1,6 @@
+package requests
+
+type Header struct {
+	Brand		int		`json:"Brand"`
+	IsReleased	*bool	`json:"IsReleased"`
+}
---
+++ DPFM_API_Caller/sql.go
@@ -0,0 +1,35 @@
+package dpfm_api_caller
+
+import (
+	dpfm_api_input_reader "data-platform-api-brand-releases-rmq-kube/DPFM_API_Input_Reader"
+	dpfm_api_output_formatter "data-platform-api-brand-releases-rmq-kube/DPFM_API_Output_Formatter"
+
+	"fmt"
+
+	"github.com/latonaio/golang-logging-library-for-data-platform/logger"
+)
+
+func (c *DPFMAPICaller) HeaderRead(
+	input *dpfm_api_input_reader.SDC,
+	log *logger.Logger,
+) *dpfm_api_output_formatter.Header {
+	where := fmt.Sprintf("WHERE header.Brand = %d ", input.Header.Brand)
+	rows, err := c.db.Query(
+		`SELECT 
+			header.Brand
+		FROM DataPlatformMastersAndTransactionsMysqlKube.data_platform_brand_header_data as header 
+		` + where + ` ;`)
+	if err != nil {
+		log.Error("%+v", err)
+		return nil
+	}
+	defer rows.Close()
+
+	data, err := dpfm_api_output_formatter.ConvertToHeader(rows)
+	if err != nil {
+		log.Error("%+v", err)
+		return nil
+	}
+
+	return data
+}
